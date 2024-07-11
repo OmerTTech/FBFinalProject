4cc02e09a5ee69ff6e3eaebc670677abca11932b
HyperOmerJ <omer.tapdiqov@gmail.com>
2024-07-11 17:54:47 +0400

    starting darkmode

--- src/components/ThemeSwitch/ThemeSwitch.jsx
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import "./ThemeSwitch.css"
+import React, { useContext } from "react";
+import "./ThemeSwitch.css";
+import { NavContext } from "../../contexts/NavContext";
 const ThemeSwitch = () => {
+  const { theme, toggleTheme } = useContext(NavContext);
+
+  const handleChange = (event) => {
+    toggleTheme();
+  };
+  
   return (
     <label className="ui-switch">
-      <input type="checkbox" id="ThemeSwitch"/>
+      <input
+        type="checkbox"
+        id="ThemeSwitch"
+        onChange={handleChange}
+        checked={theme === "dark"}
+      />
       <div className="slider">
         <div className="circle"></div>
       </div>
--- src/contexts/NavContext.js
+++ src/contexts/NavContext.js
@@ -6,12 +6,13 @@ const NavProvider = ({ children }) => {
   // Nav - Sidebar minimize and maximize button.
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 500);
+  const [theme, setTheme] = useState('light');
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-// if Screen is smaller 500px to get true else false.
+  // if Screen is smaller 500px to get true else false.
   useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth < 500);
@@ -22,8 +23,16 @@ const NavProvider = ({ children }) => {
     };
   }, []);
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <NavContext.Provider value={{ isNavOpen, toggleNav, isSmallScreen }}>
+    <NavContext.Provider value={{ isNavOpen, toggleNav, isSmallScreen, theme, toggleTheme }}>
       {children}
     </NavContext.Provider>
   );
